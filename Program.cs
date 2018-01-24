fd8ce2b0a35724990bf7ba6effeec1454c4c415a
Luis Jimenez <lj974730@wtstudents.wtamu.edu>
2018-01-24 12:12:55 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SonOfNicknameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
